0e7879ca902688cceaca14b63cd06c015f8f228e
Maykon1112 <valentimmaykon@gmail.com>
2023-01-06 15:20:22 -0300

    Atualização

--- JogodeAdivinhação.py
+++ JogodeAdivinhação.py
@@ -1,11 +1,20 @@
 import random
 
-maquina = random.randint(1,10)
+while True:
+    
+    maquina = random.randint(1,10)
 
-usuario = int(input("Digite um número de 1 a 10:\n\n"))
+    usuario = int(input("Digite um número de 1 a 10:\n\n"))
 
-if(maquina == usuario):
-    print("\nVocê ganhou o jogo você digitou o número {} e a maquina também\nParabéns!!!".format(usuario,maquina))
-else:
-    print("\nVocê perdeu você digitou o número {} e a máquina digitou o número {}\nQue pena!!!".format(usuario,maquina))
+    if(maquina == usuario):
+        print("\nVocê ganhou o jogo você digitou o número {} e a maquina também\nParabéns!!!".format(usuario,maquina))
+    else:
+        print("\nVocê perdeu você digitou o número {} e a máquina digitou o número {}\nQue pena!!!".format(usuario,maquina))
 
+    print("\nQuer continuar jogando?\n1 - Sim\n2 - Não\n")
+    resposta = int(input("\n"))
+    if(resposta == 1):
+        continue
+    elif(resposta == 2):
+        break
+    
